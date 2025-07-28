a8791633393b6f4ed5f03665801bb7707c3b88cf
Erik Santos <erikluan@alumni.usp.br>
2025-07-28 14:57:08 -0300

    Remove a pasta com as credenciais

--- conectar/credenciais.R
+++
@@ -1,6 +0,0 @@
-# Define as credenciais para acesso (login e senha: "admin")
-credentials <- data.frame(
-  user = c("admin", "recife_admin"),
-  password = c("admin", "painel_recife"),
-  stringsAsFactors = FALSE
-)
